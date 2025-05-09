be8b3c38aea8474a5f64a0c0be14018533366b7f
Santiago Alvarez <alvarezsanjavier@gmail.com>
2025-05-09 17:55:54 -0300

    Agregar archivos de proyecto.

---
+++ CloudFileStorage/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace CloudFileStorage.Models
+{
+    public class User
+    {
+        public string id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string role { get; set; }
+
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+
+
+    }
+}
